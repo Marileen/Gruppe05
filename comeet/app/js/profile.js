c884a32f31c39f393ec9221834276899dad2003b
Marileen Stamer <marileen.stamer@t8y.com>
2015-11-22 16:02:21 +0100

    Comeet - profil change logik angefangen

--- comeet/app/js/profile.js
+++ comeet/app/js/profile.js
@@ -5,15 +5,23 @@
  * das geschieht über das setzen einer css klasse
  *
  * **/
-function changeUsername (event)
+function changeProfileData (item, event)
 {
     event.preventDefault();
-    document.querySelector('.username-changed').classList.add('show');
+    document.querySelector(item).classList.add('show');
+
+    //console.log(event.target);
+    //bei pw und email auch die Buttons verschieben:
+    //if (event.target.id == 'email-change' || event.target.id == 'pw-change') {
+        event.target.classList.add('confirm-change');
+    //}
 }
 
 function initProfile ()
 {
-    document.querySelector('#username-change').addEventListener('click', changeUsername.bind(event));
+    document.querySelector('#username-change').addEventListener('click', changeProfileData.bind(null, '.username-changed', event));
+    document.querySelector('#email-change').addEventListener('click', changeProfileData.bind(event, '.email-change'));
+    document.querySelector('#pw-change').addEventListener('click', changeProfileData.bind(event, '.pw-change'));
 }
 
 window.onload=initProfile;
