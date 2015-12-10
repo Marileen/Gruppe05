a378f1cfe8860260dd04714d86d2c597cda1e956
Marileen Stamer <marileen.stamer@t8y.com>
2015-12-10 22:01:20 +0100

    - php Testdateien - event Detail Seite angepasst - event Contact Detail

---
+++ comeet/config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ 
+define ( 'MYSQL_HOST',      'mysql5.service' );
+define ( 'MYSQL_BENUTZER',  'HTO01FLQZCHT_2' );
+define ( 'MYSQL_KENNWORT',  'tj311z26' );
+define ( 'MYSQL_DATENBANK', 'HTO01FLQZCHT_2' );
+?>
---
+++ comeet/test.php
@@ -0,0 +1,33 @@
+<?php
+require_once ('config.php');
+$db_link = mysql_connect ( MYSQL_HOST, 
+                           MYSQL_BENUTZER, 
+                           MYSQL_KENNWORT );
+ 
+$db_sel = mysql_select_db( MYSQL_DATENBANK )
+   or die("Auswahl der Datenbank fehlgeschlagen");
+ 
+$sql = "SELECT * FROM Users";
+ 
+$db_erg = mysql_query( $sql );
+if ( ! $db_erg )
+{
+  die('Ungültige Abfrage: ' . mysql_error());
+}
+ 
+echo '<table border="1">';
+while ($zeile = mysql_fetch_array( $db_erg, MYSQL_ASSOC))
+{
+  echo "<tr>";
+  echo "<td>". $zeile['ID'] . "</td>";
+  echo "<td>". $zeile['Firstname'] . "</td>";
+  echo "<td>". $zeile['Lastname'] . "</td>";
+  echo "<td>". $zeile['Email'] . "</td>";
+  echo "<td>". $zeile['Birthdate'] . "</td>";
+  echo "<td>". $zeile['Picture'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+ 
+mysql_free_result( $db_erg );
+?>
