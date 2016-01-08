47780e66919b29f4fcdad5440f509ec12eb99f45
Marileen Stamer <marileen.stamer@t8y.com>
2016-01-08 21:23:18 +0100

    - Items bei fremd Events anzeigen

--- comeet/app/js/event-detail.js
+++ comeet/app/js/event-detail.js
@@ -3,10 +3,11 @@
  *
  * **/
 
-function getDetailEventData () {
+function getDetailEventData()
+{
 
     //Event Daten holen anhand der Event ID
-    eventID = 'id='+QueryString.id;
+    eventID = 'id=' + QueryString.id;
     console.log(eventID);
 
     eventRequest = makeAjaxPostRequest('event-detail.php', eventID);
@@ -14,8 +15,7 @@ function getDetailEventData () {
     eventRequest.onreadystatechange = function ()
     { //Call a function when the state changes.
 
-        if (eventRequest.readyState == 4 && eventRequest.status == 200)
-        {
+        if (eventRequest.readyState == 4 && eventRequest.status == 200) {
             console.log("Response: " + eventRequest.responseText);
 
             var entries = {};
@@ -56,8 +56,7 @@ function getDetailEventData () {
             document.getElementById('Address').innerHTML = entries.street + ' ' + entries.nr + ', ' + entries.postcode + ' ' + entries.city;
             document.getElementById('Map').setAttribute('src', entries.MapLink);
 
-        } else
-        {
+        } else{
             //redirect to login or show a message
         }
     }
@@ -65,26 +64,26 @@ function getDetailEventData () {
 }
 
 
-function attendToEvent (e) {
+function attendToEvent(e)
+{
 
     e.preventDefault();
 
     //gucken welche checkboxen angeklickt sind
     var items = document.querySelectorAll('.item-list .item');
-    var data = "id=" +  QueryString.id + "&";
+    var data = "id=" + QueryString.id + "&";
 
     Array.prototype.forEach.call(items, function (elem, idx)
     {
-        if (elem.querySelector('input').checked)
-        {
-            data = data + "item=" + elem.querySelector('input').value + "&"
+        if (elem.querySelector('input').checked) {
+            data = data + "item=" + elem.querySelector('label').innerHTML + "&"
         }
     });
 
     console.log('daten:' + data);
 
     //daten wegschicken
-    attendEventRequest = makeAjaxPostRequest('attend-event.php',data);
+    attendEventRequest = makeAjaxPostRequest('attend-event.php', data);
 
     attendEventRequest.onreadystatechange = function ()
     { //Call a function when the state changes.
@@ -92,15 +91,14 @@ function attendToEvent (e) {
         if (attendEventRequest.readyState == 4 && attendEventRequest.status == 200) {
             console.log('zurück von attend-event.php: ' + attendEventRequest.responseText);
             //meldung event gelöscht
-            if (attendEventRequest.responseText.indexOf('success') > -1)
-            {
+            if (attendEventRequest.responseText.indexOf('success') > -1) {
                 document.querySelector('.message.attend').classList.add('show');
                 document.querySelector('.attendEvent form').classList.add('hide');
 
-            } else
-            {
-                //Meldung anzeigen, dass der Login fehlgeschlagen ist
-                document.querySelector('.errormessage.attendevent').classList.add('show');
+            } else{
+                if (document.querySelector('.errormessage.attendevent')) {
+                    document.querySelector('.errormessage.attendevent').classList.add('show');
+                }
             }
         }
     }
@@ -111,7 +109,8 @@ function attendToEvent (e) {
  * Event löschen, dann wieder die Übersicht anzeigen
  *
  * **/
-function deleteEvent(e) {
+function deleteEvent(e)
+{
 
 
     e.preventDefault();
@@ -122,7 +121,7 @@ function deleteEvent(e) {
     if (deleteConfirm == true) {
         console.log('delete event bestätigt');
 
-        deleteEventRequest = makeAjaxPostRequest('delete-event.php','id=' + QueryString.id);
+        deleteEventRequest = makeAjaxPostRequest('delete-event.php', 'id=' + QueryString.id);
 
         console.log('what i sent:');
         console.log('id=' + QueryString.id);
@@ -133,19 +132,17 @@ function deleteEvent(e) {
             if (deleteEventRequest.readyState == 4 && deleteEventRequest.status == 200) {
                 console.log('zurück von delete-event.php: ' + deleteEventRequest.responseText);
                 //meldung event gelöscht
-                if (deleteEventRequest.responseText.indexOf('success') > -1)
-                {
+                if (deleteEventRequest.responseText.indexOf('success') > -1) {
                     window.location.href = '02_overview.html';
 
-                } else
-                {
+                } else{
                     //Meldung anzeigen, dass der Login fehlgeschlagen ist
                     document.querySelector('.errormessage.event').classList.add('show');
                 }
             }
         }
 
-    } else {
+    } else{
         //
     }
 
